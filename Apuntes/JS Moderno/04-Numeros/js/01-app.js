95eaea5cd9dfc4abc5a04f6595ed77250e030b36
Grillo <carlsenrmt26@gmail.com>
2022-05-26 14:15:48 -0400

    course JavaScript second day

---
+++ Apuntes/JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,17 @@
+//Creacion de números
+//No es necesario definir el tipo de numero
+
+const numero1= 30;
+const numero2= 20;
+const numero3= 20.20;
+const numero4= .102020;
+const numero5= -20;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+const numer6 = new Number(80);
+console.log(numer6);
